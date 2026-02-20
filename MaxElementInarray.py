deda6b76364ec8d37c2cc2cf4737b90e004fa38e
Batti Chandan Singh <chandhansingh854@gmail.com>
2026-02-20 23:18:17 +0530

    Max Element finding

--- MaxElementInarray.py
+++ MaxElementInarray.py
@@ -5,5 +5,5 @@ def maxelementinarry(arr):
             max_value = arr[num]
     return num+1
 arr=list(map(int,input("Enter the elements of the array: ").split()))
-print("The maximum element in the array is: ",maxelementinarry(arr))
+print("The maximum element in the array is: ",maxelementinarray(arr))
 
